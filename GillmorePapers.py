52247fa627a27a7046794c5e9576617917ab2079
ashkaneshghi <116697979+ashkaneshghi@users.noreply.github.com>
2023-04-24 16:53:52 +0100

    Update GillmorePapers.py

--- GillmorePapers.py
+++ GillmorePapers.py
@@ -5,33 +5,36 @@ from typing import List
 from sentence_transformers import SentenceTransformer
 from langchain.vectorstores import FAISS
 
-class LocalHuggingFaceEmbeddings(Embeddings):
-    def __init__(self, model_id): 
-        self.model = SentenceTransformer(model_id)
+# class LocalHuggingFaceEmbeddings(Embeddings):
+#     def __init__(self, model_id): 
+#         self.model = SentenceTransformer(model_id)
         
-    def embed_documents(self, texts: List[str]) -> List[List[float]]:
-        """Embed a list of documents using a locally running
-           Hugging Face Sentence Transformer model
-        Args:
-            texts: The list of texts to embed.
-        Returns:
-            List of embeddings, one for each text.
-        """
-        embeddings =self.model.encode(texts)
-        return embeddings
+#     def embed_documents(self, texts: List[str]) -> List[List[float]]:
+#         """Embed a list of documents using a locally running
+#            Hugging Face Sentence Transformer model
+#         Args:
+#             texts: The list of texts to embed.
+#         Returns:
+#             List of embeddings, one for each text.
+#         """
+#         embeddings =self.model.encode(texts)
+#         return embeddings
 
-    def embed_query(self, text: str) -> List[float]:
-        """Embed a query using a locally running HF 
-        Sentence trnsformer. 
-        Args:
-            text: The text to embed.
-        Returns:
-            Embeddings for the text.
-        """
-        embedding = self.model.encode(text)
-        return list(map(float, embedding))
+#     def embed_query(self, text: str) -> List[float]:
+#         """Embed a query using a locally running HF 
+#         Sentence trnsformer. 
+#         Args:
+#             text: The text to embed.
+#         Returns:
+#             Embeddings for the text.
+#         """
+#         embedding = self.model.encode(text)
+#         return list(map(float, embedding))
 
-embeddings = LocalHuggingFaceEmbeddings('all-mpnet-base-v2')
+# embeddings = LocalHuggingFaceEmbeddings('all-mpnet-base-v2')
+
+from langchain.embeddings import HuggingFaceEmbeddings
+embeddings = HuggingFaceEmbeddings()
 
 vector_store = FAISS.load_local("GillPapers_index", embeddings)
 
